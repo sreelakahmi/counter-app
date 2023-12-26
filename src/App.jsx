1217fe294a57f2b26fad6f17af84a0fffe7d0f7d
sreelakshmi <sreelakshmi1636@gmail.com>
2023-12-27 00:16:23 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Counter from './components/Counter'
+
+function App() {
+  return (
+    <div style={{height:'100vh'}}className='w-100 d-flex justify-content-center align-items-center mt-5'><Counter/></div>
+  )
+}
+
+export default App
